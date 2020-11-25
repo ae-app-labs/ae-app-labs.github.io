cb5a4671278db71aae49ca6eef922111417b6d37
midhunhk <2026817+midhunhk@users.noreply.github.com>
2020-11-24 22:03:31 -0500

    try to add some animations

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Organization Page
 
 v2 of the site is built with sapper and bulma  
 Will need to be upgraded to svelte/kit once it is ready.
+
+## Styles
+Bulma  
+
+## Animations
+https://animate.style/  
+https://wowjs.uk/docs.html  
--- src/components/CategorySection.svelte
+++ src/components/CategorySection.svelte
@@ -6,13 +6,14 @@
     $: projects = $sectionData.projects
 </script>
 
-<section class="section" id="projects-{$sectionData.category}" name="projects-{$sectionData.category}">
+<section class="section wow slideInLeft" id="projects-{$sectionData.category}" name="projects-{$sectionData.category}"
+         data-wow-duration="2s" data-wow-delay="5s">
 	<div class="container is-fluid">
 		<div class="subtitle section-title">{$sectionData.categoryTitle}</div>
 		
 		<div class="columns is-multiline" >
             {#each projects as project (project.id)}
-			    <div class="column is-half">
+			    <div class="column is-half wow fadeInUp" data-wow-duration="2s" data-wow-delay="5s">
                     <Project projectData={project}/>
 		        </div>
             {/each}
