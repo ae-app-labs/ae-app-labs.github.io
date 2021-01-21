1fa39b965eb4dc5e004646c69bed65c42c0fbdd0
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-01-21 16:42:20 -0500

    expose webProjects from the store

--- src/data/store.js
+++ src/data/store.js
@@ -2,6 +2,7 @@ import { readable } from "svelte/store"
 
 import desktopProjectsData from './projects-desktop.js'
 import androidProjectsData from './projects-android.js'
+import webProjectsData from './projects-web.js'
 
 export const desktopProjects = readable(null, set => {
     set(desktopProjectsData)
@@ -9,4 +10,8 @@ export const desktopProjects = readable(null, set => {
 
 export const androidProjects = readable(null, set => {
     set(androidProjectsData)
+})
+
+export const webProjects = readable(null, set => {
+    set(webProjectsData)
 })
