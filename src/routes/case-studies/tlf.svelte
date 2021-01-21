9e63f73d98e97bf6a69a01c52df8121616ea4e6a
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-01-20 23:02:40 -0500

    update article

--- src/routes/case-studies/tlf.svelte
+++ src/routes/case-studies/tlf.svelte
@@ -75,10 +75,10 @@
     <img src="/img/case-studies/tlf/tlf_wireframes.png" alt="wireframes" />
 </section>
 
-<section class="section">
+<section class="section mt-2 mb-6">
 	<div class="container">
 		<div class="columns is-vcentered">
-			<div class="column is-6 is-hidden-mobile">
+			<div class="column is-8 is-hidden-mobile">
                 &nbsp;
             </div>
 			<div class="column is-4 stagger2">
@@ -95,10 +95,13 @@
     <img src="/img/case-studies/tlf/tlf_mockups.png" alt="mockups" />
 </section>
 
-<section class="section">
+<section class="section mt-2 mb-6">
 	<div class="container">
 		<div class="columns is-vcentered">
-			<div class="column is-5 stagger2">
+			<div class="column is-6 is-hidden-mobile">
+                &nbsp;
+            </div>
+			<div class="column is-4 stagger2">
                 <p class="is-size-4 mb-4 has-text-weight-bold">Design Mockup</p>
 				<p class="is-size-5">The second step in design was to create a mockup based on the approved wireframes.</p> <br/>
                 <p class="is-size-6">Stock images were used, and we finally made use of the color palette.</p>
@@ -107,11 +110,53 @@
 	</div>
 </section>
 
+<!-- Section - Mission -->
+<section class="section section-full-width">
+    <img src="/img/case-studies/tlf/section_mission.jpg" alt="mission section" />
+</section>
+
+<section class="section mt-2 mb-6">
+	<div class="container">
+		<div class="columns is-vcentered">
+			<div class="column is-4 stagger2 is-offset-2">
+                <p class="is-size-4 mb-4 has-text-weight-bold">The Mission Section</p>
+				<p class="is-size-5">The organization works under five main tenets, 
+					and we decided to signify each with a unique color from the palette which was created from the logo.</p> <br/>
+                <p class="is-size-6">A number of ideas were bounced around to incorporate this, and finally selected the above design.</p>
+            </div>
+			<div class="column is-6 stagger2 p-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="264.588" height="206.216" viewBox="0 0 264.588 206.216"><g transform="translate(-750.762 -187.108)"><path d="M-1111.191,10567.267s276.272,6.585,241.531-199.583" transform="translate(1864 -10176)" fill="none" stroke="#db3069" stroke-linecap="round" stroke-width="4" stroke-dasharray="12 13"/><path d="M-637.484,10411.053l14.511-16.557,17.953,16.557" transform="translate(1617.543 -10204.526)" fill="none" stroke="#db3069" stroke-linecap="round" stroke-width="4"/></g></svg>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Section - Final Design -->
+<section class="section section-full-width">
+    <img src="/img/case-studies/tlf/tlf_final_design.png" alt="final design" />
+</section>
+
+<section class="section mt-2">
+	<div class="container">
+		<div class="columns is-vcentered">
+			<div class="column is-4 is-hidden-mobile">
+                &nbsp;
+            </div>
+			<div class="column is-4 stagger2">
+                <p class="is-size-4 mb-4 has-text-weight-bold">The Final Design</p>
+				<p class="is-size-5">Finally, once the mockups were approved we began to develop the site incorporating a few suggestions.</p> <br/>
+                <p class="is-size-6">The site is powered by Wordpress, using a custom layout engine. Wordpress helps with the content management and ease of customization.</p>
+            </div>
+		</div>
+	</div>
+</section>
+
 <section class="section">
 	<div class="container">
-		<div class="columns">
-			<div class="column is-half">&nbsp;</div>
-			<div class="column is-half"></div>
+		<div class="columns is-vcentered">
+			<div class="column has-text-centered">
+				<a href="https://www.thelotusfoundation.org/" class="button is-primary" target="_blank">Visit Project</a>
+			</div>
 		</div>
 	</div>
 </section>
