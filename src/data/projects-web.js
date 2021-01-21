2f6650bd88e9308559aa39feaea7d3628413a33c
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-01-21 16:40:26 -0500

    add data for web projects

---
+++ src/data/projects-web.js
@@ -0,0 +1,38 @@
+const webProjectsData = {
+    "categoryId": "3",
+    "category": "web",
+    "categoryTitle": "Web Projects",
+    "projects": [
+      {
+        "id": "301",
+        "icon": "\\img\\portfolio\\icons\\icon_triton_player.png",
+        "title": "tlf",
+        "featured": true,
+        "featuredImage": "\\img\\portfolio\\desktop\\project_screenshot_triton.png",
+        "tags": [
+            {
+                "id":"1",
+                "name":"Wordpress",
+                "className": "is-primary"
+            }
+        ],
+        "description": "We designed and developed the website for a non profit organization.",
+        "buttons": [
+          {
+            "id": "330101",
+            "isPrimary": false,
+            "text": "Case Study",
+            "target": "https://ae-app-labs.github.io/case-studies/tlf"
+          },
+          {
+            "id": "330102",
+            "isPrimary": true,
+            "text": "View Project",
+            "target": "https://www.thelotusfoundation.org"
+          }
+        ]
+      }
+    ]
+  }
+
+  export default webProjectsData
