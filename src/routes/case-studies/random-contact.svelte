141a7118ffd35f3dd5c508b49a7c26fbd28c45b3
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-03-18 22:10:01 -0400

    update button

--- src/routes/case-studies/random-contact.svelte
+++ src/routes/case-studies/random-contact.svelte
@@ -135,9 +135,9 @@
 	<div class="container">
 		<div class="columns is-vcentered">
 			<div class="column has-text-centered">
-				<form target="_blank" action="https://play.google.com/store/apps/details?id=com.ae.apps.randomcontact">
-					<button class="button is-warning is-medium p-2 btn btn-hover-effect">Play Store</button>
-				</form>
+                <a href="https://play.google.com/store/apps/details?id=com.ae.apps.randomcontact" rel="nofollow" target="_blank">
+                    <img alt="Get it on Google Play" width="200px" src="https://camo.githubusercontent.com/2149f526e69167218eb7eea8f21cb74a756aa43495f7acfeccfe995d40f62028/68747470733a2f2f706c61792e676f6f676c652e636f6d2f696e746c2f656e5f75732f6261646765732f696d616765732f67656e657269632f656e5f62616467655f7765625f67656e657269632e706e67" data-canonical-src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" style="max-width:100%;">
+                </a>
 			</div>
 		</div>
 	</div>
