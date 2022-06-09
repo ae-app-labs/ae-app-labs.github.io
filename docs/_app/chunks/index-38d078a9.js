e26320d8e74a42fd2c8a7d1d19a05219b2a158e8
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-06-09 17:51:51 -0400

    update docs

---
+++ docs/_app/chunks/index-38d078a9.js
@@ -0,0 +1,1 @@
+import{Q as n}from"./index-f1317a0d.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
