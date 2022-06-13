17f89575a8859e8d65df3981bd9f4ee1823bfae1
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-06-13 00:17:27 -0400

    update docs

--- docs/_app/chunks/index-6b8519b6.js
+++
@@ -1,1 +0,0 @@
-import{Q as n}from"./index-ecdd3440.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
