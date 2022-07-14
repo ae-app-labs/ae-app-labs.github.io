b6f4229339b77d6f63bcd8818ebc760cf2c23b9c
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-07-14 01:14:12 -0400

    update recent work template

--- src/lib/components/RecentWork.svelte
+++ src/lib/components/RecentWork.svelte
@@ -7,30 +7,30 @@
         <div class="card-image grow-effect">
             <a href="https://www.uplabs.com/posts/eco-awareness-website" target="_blank" alt="view on uplabs">
                 <figure class="image m-0 p-0">
-                    <img src="/img/portfolio/01-eco-website.png" alt="muyu web" class="v-lazy-image v-lazy-image-loaded">
+                    <img src="/img/portfolio/web_eco_awareness.png" alt="eco awareness web" class="v-lazy-image v-lazy-image-loaded">
                 </figure>
             </a>
         </div>
     </div>
     <!-- End Column 1-->
+    <div class="column is-one-third">
+        <div class="card-image grow-effect">
+            <a href="https://www.behance.net/gallery/148120951/Concerto" target="_blank" alt="view on uplabs">
+                <figure class="image m-0 p-0">
+                    <img src="/img/portfolio/web_concert.png" alt="concerto" class="v-lazy-image v-lazy-image-loaded">
+                </figure>
+            </a>
+        </div>
+    </div>
 
     <div class="column is-one-third">
         <div class="card-image grow-effect">
             <a href="https://www.uplabs.com/posts/adventure-next-landing-page-concept" target="_blank" alt="view on uplabs">
                 <figure class="image m-0 p-0">
-                    <img src="/img/portfolio/02-adventure-next.png" alt="muyu web" class="v-lazy-image v-lazy-image-loaded">
+                    <img src="/img/portfolio/web_adventure_next.png" alt="adventure next web" class="v-lazy-image v-lazy-image-loaded">
                 </figure>
             </a>
         </div>
     </div>
 
-    <div class="column is-one-third">
-        <div class="card-image grow-effect">
-            <a href="https://www.uplabs.com/posts/website-for-a-virtual-job-board-app" target="_blank" alt="view on uplabs">
-                <figure class="image m-0 p-0">
-                    <img src="/img/portfolio/03-product-one.png" alt="muyu web" class="v-lazy-image v-lazy-image-loaded">
-                </figure>
-            </a>
-        </div>
-    </div>
 </div>
