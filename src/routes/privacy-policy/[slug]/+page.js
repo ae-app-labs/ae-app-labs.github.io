71a65583859ecd5da7479c13534237e16e70f888
midhunhk <2026817+midhunhk@users.noreply.github.com>
2022-12-29 12:42:38 -0500

    upgrade dynamic content fetching

--- src/routes/privacy-policy/[slug]/+page.js
+++ src/routes/privacy-policy/[slug]/+page.js
@@ -1,14 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 
+export async function load( { fetch, params} ) {
+	const res = await fetch(`/privacy-policy/${params.slug}.json`)
+	const policy = await res.json()
 
-	export async function load( { fetch, params} ) {
-		const res = await fetch(`/privacy-policy/${params.slug}.json`)
-const policy = await res.json()
-		
-		if(res.ok) {
-    return {
-    policy
+	if(res.ok) {
+		return {
+			policy
+		}
+	} 
+	throw redirect(301, '/privacy-policy');
 }
-} 
-throw redirect(301, '/privacy-policy');
-	}
--- src/routes/privacy-policy/[slug]/+page.svelte
+++ src/routes/privacy-policy/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 	import ContactForm from "$lib/components/ContactForm.svelte";
 	export let data
 
-	$: post = data.post
+	$: post = data.policy.post
 </script>
 
 <style>
