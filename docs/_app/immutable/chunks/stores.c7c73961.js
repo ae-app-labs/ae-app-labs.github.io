b8339ab327efff93c35e2d4f42ca0e5e2e7865b4
midhunhk <2026817+midhunhk@users.noreply.github.com>
2023-12-14 15:01:20 -0500

    update docs

--- docs/_app/immutable/chunks/stores.c7c73961.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.77ad2cb8.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
