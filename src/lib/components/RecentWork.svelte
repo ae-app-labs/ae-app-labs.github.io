55f8d46d11c826ea3b3d259c67c0b87813383220
midhunhk <2026817+midhunhk@users.noreply.github.com>
2024-03-03 23:06:30 -0500

    add design item

--- src/lib/components/RecentWork.svelte
+++ src/lib/components/RecentWork.svelte
@@ -5,14 +5,15 @@
     <!-- Column 1-->
     <div class="column is-one-third">
         <div class="card-image grow-effect">
-            <a href="https://www.uplabs.com/posts/eco-awareness-website" target="_blank" alt="view on uplabs" rel="noreferrer">
+            <a href="https://www.behance.net/gallery/188139897/Eco-Website" target="_blank" alt="view on uplabs" rel="noreferrer">
                 <figure class="image m-0 p-0">
-                    <img src="/img/portfolio/web_eco_awareness.png" alt="eco awareness web" class="v-lazy-image v-lazy-image-loaded">
+                    <img src="/img/portfolio/web_eco_02.png" alt="eco awareness web" class="v-lazy-image v-lazy-image-loaded">
                 </figure>
             </a>
         </div>
     </div>
     <!-- End Column 1-->
+    
     <div class="column is-one-third">
         <div class="card-image grow-effect">
             <a href="https://www.behance.net/gallery/148120951/Concerto" target="_blank" alt="view on uplabs" rel="noreferrer">
